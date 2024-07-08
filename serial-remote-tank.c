4ee2329c0c712429e517835ed2b55e6406c2a340
JoseArantes83 <josearantes20@hotmail.com>
2024-07-08 09:35:18 -0300

    fix .gitignore

--- serial-remote-tank.c
+++ serial-remote-tank.c
@@ -1,8 +1,10 @@
 #include <serial-remote-tank.h>
 #include <lcd_8bits_4x20_Mod.c>
 
-int1 processo_ligado = 0;
+int1 processo_ligado = 0, fimTempo = 0;
 int1 valvulaEntrada = 0, valvulaSaida = 0, heater = 0, cooler = 0, mixer = 0, sensorAlto = 0, sensorBaixo = 0;
+int1  temperaturaAtual = 0, temperaturaSP = 0, temperaturaBaixa = 0, temperaturaAlta = 0,
+int8 tela = 1, volume = 0, contador = 0, temperaturaMinima = 0, temperaturaMaxima = 0;
 
 void abreValvulaEntrada();
 void fechaValvulaEntrada();
@@ -17,10 +19,18 @@ void desligaMixer();
 int1 getSensorBaixo();
 int16 getTemperatura();
 int1 getSensorAlto();
+void mostraDados(int8 num_tela);
+void mudaTela();
 
 #INT_TIMER0
 void TIMER0_isr(void)
 {
+   contador++;
+   if (contador == 100)
+   {
+      contador = 0;
+      fimTempo = 1;
+   }
 }
 
 void main()
@@ -31,15 +41,55 @@ void main()
    enable_interrupts(GLOBAL);
 
    lcd_init();
-   
-   lcd_gotoxy(1, 1);
-   printf(lcd_write_dat, "Pwr:");
-   
 
    while (TRUE)
    {
-      lcd_gotoxy(5, 1);
-      printf(lcd_write_dat, "%i", processo_ligado);
+
+      if (fimTempo)
+      {
+         fimTempo = 0;
+
+         switch (tela)
+         {
+         case 1:
+            lcd_gotoxy(1, 1);
+            printf(lcd_write_dat, "Pwr:");
+            lcd_gotoxy(1, 2);
+            printf(lcd_write_dat, "ValEnt:  ValSai:");
+            lcd_gotoxy(1, 3);
+            printf(lcd_write_dat, "Heater:  Cooler:");
+            lcd_gotoxy(1, 4);
+            printf(lcd_write_dat, "Mixer:");
+            mostraDados(1);
+            break;
+         case 2:
+            lcd_gotoxy(1, 1);
+            printf(lcd_write_dat, "NvlAlt:  NvlBai:");
+            lcd_gotoxy(1, 2);
+            printf(lcd_write_dat, "TempBai:  TempAlt:");
+            lcd_gotoxy(1, 3);
+            printf(lcd_write_dat, "Volume:  Cooler:");
+            lcd_gotoxy(1, 4);
+            printf(lcd_write_dat, "TempAtua:  TempSP:");
+            mostraDados(2);
+            break;
+         case 3:
+            lcd_gotoxy(1, 1);
+            printf(lcd_write_dat, "Histerese: ");
+            lcd_gotoxy(1, 2);
+            printf(lcd_write_dat, "TempoMistura: ");
+            lcd_gotoxy(1, 3);
+            printf(lcd_write_dat, "TempMin:");
+            lcd_gotoxy(1, 4);
+            printf(lcd_write_dat, "TempMax:");
+            mostraDados(3);
+            break;
+         case 4:
+            mostraDados(4);
+            break;
+         }
+      }
+
       // criar booleanos de estado
       // testar se t� vazio
       // se n tive esvazia
@@ -54,6 +104,82 @@ void main()
       // desliga heater
       // desliga mixer
       // se sensor baixo for 0, desliga valvula baixo e come�a de novo.
+   }
+}
+
+void mudaTela()
+{
+   tela++;
+   if (tela > 4)
+   {
+      tela = 1;
+   }
+}
+
+void mostraDados(int8 num_tela)
+{
+   switch (num_tela)
+   {
+   case 1:
+      lcd_gotoxy(5, 1);
+      printf(lcd_write_dat, "%i", processo_ligado);
+      lcd_gotoxy(8, 2);
+      printf(lcd_write_dat, "%i", valvulaEntrada);
+      lcd_gotoxy(17, 2);
+      printf(lcd_write_dat, "%i", valvulaSaida);
+      lcd_gotoxy(8, 3);
+      printf(lcd_write_dat, "%i", heater);
+      lcd_gotoxy(17, 3);
+      printf(lcd_write_dat, "%i", cooler);
+      lcd_gotoxy(7, 4);
+      printf(lcd_write_dat, "%i", mixer);
+      break;
+   case 2:
+      lcd_gotoxy(8, 1);
+      printf(lcd_write_dat, "%i", sensorAlto);
+      lcd_gotoxy(18, 1);
+      printf(lcd_write_dat, "%i", sensorBaixo);
+      lcd_gotoxy(9, 2);
+      printf(lcd_write_dat, "%i", temperaturaMinima);
+      lcd_gotoxy(19, 2);
+      printf(lcd_write_dat, "%i", temperaturaMaxima);
+      lcd_gotoxy(8, 3);
+      printf(lcd_write_dat, "%i", volume);
+      lcd_gotoxy(16, 3);
+      printf(lcd_write_dat, "%i", cooler);
+      lcd_gotoxy(7, 4);
+      printf(lcd_write_dat, "%i", temperaturaAtual);
+      lcd_gotoxy(17, 4);
+      printf(lcd_write_dat, "%i", temperaturaSP);
+      break;
+   case 3:
+      lcd_gotoxy(8, 1);
+      printf(lcd_write_dat, "%i", sensorAlto);
+      lcd_gotoxy(18, 1);
+      printf(lcd_write_dat, "%i", sensorBaixo);
+      lcd_gotoxy(9, 2);
+      printf(lcd_write_dat, "%i", temperaturaMinima);
+      lcd_gotoxy(19, 2);
+      printf(lcd_write_dat, "%i", temperaturaMaxima);
+      lcd_gotoxy(8, 3);
+      printf(lcd_write_dat, "%i", volume);
+      lcd_gotoxy(16, 4);
+      printf(lcd_write_dat, "%i", cooler);
+      break;
+   case 4:
+      lcd_gotoxy(5, 1);
+      printf(lcd_write_dat, "");
+      lcd_gotoxy(8, 2);
+      printf(lcd_write_dat, "");
+      lcd_gotoxy(17, 2);
+      printf(lcd_write_dat, "");
+      lcd_gotoxy(8, 3);
+      printf(lcd_write_dat, "");
+      lcd_gotoxy(17, 3);
+      printf(lcd_write_dat, "");
+      lcd_gotoxy(7, 4);
+      printf(lcd_write_dat, "");
+      break;
    }
 }
 
